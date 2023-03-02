7563cc9a0f3e5391b7cb9933babbc0bce870dd58
marzglav <feg.leg.crypto@gmail.com>
2023-03-02 20:01:16 +0100

    Hooks and scss

--- src/view/App/index.jsx
+++ src/view/App/index.jsx
@@ -1,29 +1,46 @@
 import Header from "../../components/Header";
 import List from "../../components/List";
+import { useState } from "react";
 
 import "./style.scss";
 
+const _firstResults = [
+  { name: "Ante", surname: "Antic", percentage: 80 },
+  { name: "Mate", surname: "Matic", percentage: 50 },
+  { name: "Antea", surname: "Antic", percentage: 100 },
+];
+
 function App() {
-  const firstResults = [
-    { name: "Ante", surname: "Antic", percentage: 80 },
-    { name: "Mate", surname: "Matic", percentage: 50 },
-    { name: "Antea", surname: "Antic", percentage: 100 },
-  ];
+  const [firstResults, setFirstResults] = useState(_firstResults);
   const secondResults = [
     { name: "Ante", surname: "Antic", percentage: 10 },
     { name: "Mate", surname: "Matic", percentage: 20 },
     { name: "Antea", surname: "Antic", percentage: 50 },
   ];
+  const inputHandler = (event) => {
+    const filter = event.target.value;
+    const filteredResults = _firstResults.filter(({ name, surname }) => {
+      const value = `${name}${surname}`.toLowerCase();
+
+      return value.includes(filter.toLowerCase());
+    });
+
+    console.log("Handler", filteredResults);
+    setFirstResults(filteredResults);
+  };
+
+  console.log("Render", firstResults);
 
   return (
     // React fragment <> </>
     <>
       <Header who="world" others="others" />
       <section className="main-container">
+        <input type="text" onChange={inputHandler} />
         <span>Prvi kolokvij</span>
         <List data={firstResults} />
-        <span>Drugi kolokvij</span>
-        <List data={secondResults} />
+        {/* <span>Drugi kolokvij</span>
+        <List data={secondResults} /> */}
       </section>
     </>
   );
